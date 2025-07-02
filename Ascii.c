e65578b3a271bed6a26078ffa66e9c08abfe0ee0
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-07-02 19:16:40 +0530

    firts commit

---
+++ Ascii.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+     char ch;
+     printf("enter a charater");
+     scanf("%c",&ch);
+
+     printf("Ascii value are '%c' is  %d\n",ch,ch);
+     return 0;
+     
+}
