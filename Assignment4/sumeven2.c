57ca36de97a2b3668acd5b617b5be59f79919be4
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-07-09 23:18:30 +0530

    first commit

---
+++ Assignment4/sumeven2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+     int i,sum=0;
+     for(i=1; i<=10; i++){
+          if(i % 2 == 0)
+
+          printf("Sum of even numbers from 1 to 10 is: %d\n", sum);
+     }
+     return 0;
+}
