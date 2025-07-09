2b832294ca0058803810320dd2d91b48b71caaca
Ajay prakash pipare <144015485+Ajaypipare@users.noreply.github.com>
2025-07-09 22:08:39 +0530

    first commit

---
+++ Assignment4/sumnumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+     int i ,sum=0;
+     for(i=100; i<=150 ;i++)
+     // for(i=1; i<=5; i++)
+     
+     {
+
+          sum=sum+i;
+     }
+     printf("sum of number 100 to 150 is %d",sum);
+
+     return 0;
+}
